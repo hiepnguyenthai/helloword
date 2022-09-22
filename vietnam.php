5f8720579209c42172d23a6fa59bc3ea60bfa2cd
hiepnguyenthai <HiepNTGCH200524@fpt.edu.vn>
2022-09-22 16:51:51 +0700

    x

--- vietnam.php
+++ vietnam.php
@@ -5,9 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>vietnam</title>
+    <style>
+        body {
+            background-color: lavenderblush;
+        }
+    </style>
+    <link rel="stylesheet" href="css/style.css">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 <body>
-<h1>Bộ tộc Mixigaming</h1>
+<h1 style="color:red;"> Bộ tộc Mixigaming</h1>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/TK3s1PJuyws" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </body>
 </html>
